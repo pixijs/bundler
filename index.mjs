b903678586015626de7a0a291aad6eac586599d3
Matt Karl <matt@mattkarl.com>
2022-11-14 13:34:33 -0500

    Add aliases

--- index.mjs
+++ index.mjs
@@ -14,7 +14,9 @@ class Package {
         this.dependencies = null;
         this.external = !this.name.startsWith('@pixi/') && !this.name.startsWith('pixi.js');
         this.libPath = !this.external ? path.join(this.basePath, 'lib') : null;
-        this.targetName = !this.external ? Package.replacements[this.name] ?? this.name.replace('@pixi/', '') : null;
+        this.targetName = Package.aliases[this.name] || '';
+        this.targetRelativeName = this.targetName.slice(this.targetName.indexOf('/') + 1);
+        this.targetPath = this.targetRelativeName.replace(/\//g, '-');
     }
     async load() {
         this.info = await fs.readJson(path.join(this.basePath, 'package.json'));
@@ -26,16 +28,16 @@ class Package {
 
     /** Get the export inputs */
     getExports() {
-        return { ['./' + this.targetName]: JSON.parse(
+        return { ['./' + this.targetRelativeName]: JSON.parse(
             JSON.stringify(this.info.exports['.'])
-                .replace(/\/lib\//g, `/${this.targetName}/`)
+                .replace(/\/lib\//g, `/${this.targetPath}/`)
             )
         };
     }
 
     /** Copy the library the output destination */
     async copy(dest) {
-        const targetPath = path.join(dest, this.targetName);
+        const targetPath = path.join(dest, this.targetPath);
         await fs.copy(this.libPath, targetPath);
         // Copy the global.d.ts file if we have it
         const globalPath = path.join(this.basePath, 'global.d.ts');
@@ -49,11 +51,11 @@ class Package {
         }
     }
 
-    static replacements = {
-        'pixi.js': 'browser',
-        'pixi.js-legacy': 'browser-legacy',
-    };
+    /** The cache of all packages found in dependency tree */
     static packages = new Map();
+
+    /** Map of old packages names to new package names */
+    static aliases = null;
 
     /**
      * Create a new package data object.
@@ -64,6 +66,9 @@ class Package {
      * @returns 
      */
     static async create(parent, packageName, version, basePath) {
+        if (!Package.aliases) {
+            throw new Error('Package aliases not loaded');
+        }
         if (!this.packages.has(packageName)) {
             const pkg = new Package(parent, packageName, version, basePath);
             await pkg.load();
@@ -72,6 +77,13 @@ class Package {
         return Package.packages.get(packageName);
     }
 }
+
+// Create the output directory
+console.log('Create output directory');
+const outputPath = path.resolve('dist');
+await fs.remove(outputPath);
+await fs.copy(path.resolve('src'), outputPath);
+Package.aliases = await fs.readJson(path.resolve(outputPath, 'aliases.json'));
 
 // Create a collection of packages
 console.log('Source all packages');
@@ -88,25 +100,28 @@ const dependencies = packages
     .filter(pkg => pkg.external && !pkg.parent.external)
     .reduce((acc, pkg) => ({...acc, [pkg.name]: pkg.version}), {});
 
-// Create the output directory
-console.log('Create output directory');
-const outputPath = path.resolve('dist');
-await fs.remove(outputPath);
-await fs.copy(path.resolve('src'), outputPath);
-
 const libraries = packages.filter(pkg => !pkg.external);
 
 // Update the dist package.json
 console.log('Generate package.json');
 const defaultPackage = packages.find(pkg => pkg.name === 'pixi.js');
-const publishInfo = Object.assign(
-    await fs.readJson(path.resolve(outputPath, 'package.json')), {
+const publishInfo = await fs.readJson(path.resolve(outputPath, 'package.json'));
+Object.assign(
+    publishInfo, {
         dependencies,
         version: defaultPackage.info.version,
-        exports: libraries.reduce((acc, pkg) => ({...acc, ...pkg.getExports() }), {}),
-        files: libraries.map(pkg => pkg.targetName),
+        exports: libraries
+            .reduce((acc, pkg) => ({...acc, ...pkg.getExports() }),
+            publishInfo.exports
+        ),
+        files: [...publishInfo.files, ...libraries.map(pkg => pkg.targetPath)].sort(),
     }
 );
+publishInfo.exports['.'] = publishInfo.exports['./browser'];
+
+// Sort export alphabetically
+publishInfo.exports = Object.keys(publishInfo.exports).sort()
+    .reduce((acc, key) => ({ ...acc, [key]: publishInfo.exports[key] }), {});
 
 // Create the public package.json
 await fs.writeJSON(path.join(outputPath, 'package.json'), publishInfo, { spaces: 2 });
@@ -120,12 +135,43 @@ const baseReplaceOptions = {
     optionsForFiles: {
         "ignore": [
             "package.json",
+            "aliases.json",
             "README.md"
         ]
     }
 };
 // Replace internal package name requests
 console.log('Patch internal package names');
+
+// Convert @pixi/filter-alpha to pixijs/filter/alpha
+await replaceInFiles({
+    ...baseReplaceOptions,
+    from: /@pixi\/filter-([\w-]+)/g,
+    to: `${publishInfo.name}/filter/$1`,
+});
+
+// Convert @pixi/math-extras to pixijs/math/extras
+await replaceInFiles({
+    ...baseReplaceOptions,
+    from: /@pixi\/(mesh|graphics|math)-(extras)/g,
+    to: `${publishInfo.name}/$1/$2`,
+});
+
+// Convert @pixi/mixin-cache-as-bitmap to pixijs/display/cache-as-bitmap
+await replaceInFiles({
+    ...baseReplaceOptions,
+    from: /@pixi\/mixin-([\w-]+)/g,
+    to: `${publishInfo.name}/display/$1`,
+});
+
+// Convert @pixi/canvas-sprite to pixijs/sprite/canvas
+await replaceInFiles({
+    ...baseReplaceOptions,
+    from: /@pixi\/(canvas)-([\w-]+)/g,
+    to: `${publishInfo.name}/$2/$1`,
+});
+
+// Convert all other packages to pixijs using the same name
 await replaceInFiles({
     ...baseReplaceOptions,
     from: /@pixi\//g,
@@ -136,14 +182,14 @@ await replaceInFiles({
 await replaceInFiles({
     ...baseReplaceOptions,
     from: /pixi.js-legacy/g,
-    to: `${publishInfo.name}/${Package.replacements['pixi.js-legacy']}`,
+    to: Package.aliases['pixi.js-legacy'],
 });
 
 // Change pixi.js to new package name
 await replaceInFiles({
     ...baseReplaceOptions,
     from: /pixi.js/g,
-    to: `${publishInfo.name}/${Package.replacements['pixi.js']}`,
+    to: Package.aliases['pixi.js'],
 });
 
 console.log('Package output');
